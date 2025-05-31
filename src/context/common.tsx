11ccdfd075774ab78fe1fbf4029ade7e02eda296
LauraBLei <laura.blei@outlook.com>
2025-05-31 15:05:30 +0200

    fix: typeerror

--- src/context/common.tsx
+++ src/context/common.tsx
@@ -1,16 +1,34 @@
 import { useState } from "react";
 import { CommonContext } from "../types/context";
 import { Project } from "../types/types";
+import { schoolProjects } from "../Data/schoolProject";
 
 type ContextProviderProps = {
   children: React.ReactNode;
 };
 
+const project = schoolProjects.projectData[0]
+  ? schoolProjects.projectData[0]
+  : {
+      name: "",
+      "web development tools": [],
+      "completion date": "",
+      "repo-link": "",
+      "livepage-link": "",
+      image: { src: "", alt: "" },
+      video: {
+        src: "",
+        poster: "",
+      },
+      id: "",
+      text: "",
+    };
+
 export const DataProvider = ({ children }: ContextProviderProps) => {
   const [headerOpen, setHeaderOpen] = useState(false);
 
   const [projectModalOpen, setProjectModalOpen] = useState(false);
-  const [selectedProject, setSelectedProject] = useState<Project>();
+  const [selectedProject, setSelectedProject] = useState<Project>(project);
 
   return (
     <CommonContext.Provider
