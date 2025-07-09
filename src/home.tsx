55ff2c81a5013a480dc655047fbd6f4598f278a3
LauraBLei <laura.blei@outlook.com>
2025-07-09 13:50:14 +0200

    added some spacing between sections

--- src/home.tsx
+++ src/home.tsx
@@ -5,7 +5,7 @@ import { DevToolsSection } from "./Sections/dev-tools";
 
 export const HomePage = () => {
   return (
-    <div className=" h-auto max-w-[1440px] overflow-x-hidden touch-pan-y">
+    <div className=" h-auto max-w-[1440px] overflow-x-hidden flex flex-col gap-10 md:gap-20 lg:gap-40 touch-pan-y">
       <section id="welcome" className="h-dvh">
         <WelcomeSection />
       </section>
@@ -15,7 +15,7 @@ export const HomePage = () => {
       <section id="about" className=" pt-20 md:pt-30">
         <AboutSection />
       </section>
-      <section id="dev-tools" className=" pt-20 md:pt-30">
+      <section id="dev-tools" className=" pt-20 md:pt-30 mb-20 md:mb-40">
         <DevToolsSection />
       </section>
     </div>
