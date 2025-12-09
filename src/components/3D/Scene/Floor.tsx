ed7f2fa17d5cf7404ad1aa635a6f43fe89791f40
LauraBLei <laura.blei@outlook.com>
2025-12-09 19:09:11 +0100

    fixed 3D error

--- src/components/3D/Scene/Floor.tsx
+++ src/components/3D/Scene/Floor.tsx
@@ -7,21 +7,21 @@ export const Floor = ({ FloorHeight }: { FloorHeight: number }) => {
         <planeGeometry args={[8, 6]} />
         <meshBasicMaterial color="black" />
       </mesh>
-      <BlenderObject src="/models/floor.glb" position={[-4, FloorHeight, 0]} />
-      <BlenderObject src="/models/floor.glb" position={[-2, FloorHeight, 0]} />
-      <BlenderObject src="/models/floor.glb" position={[0, FloorHeight, 0]} />
-      <BlenderObject src="/models/floor.glb" position={[2, FloorHeight, 0]} />
-      <BlenderObject src="/models/floor.glb" position={[4, FloorHeight, 0]} />
-      <BlenderObject src="/models/floor.glb" position={[-4, FloorHeight, 2]} />
-      <BlenderObject src="/models/floor.glb" position={[-2, FloorHeight, 2]} />
-      <BlenderObject src="/models/floor.glb" position={[0, FloorHeight, 2]} />
-      <BlenderObject src="/models/floor.glb" position={[2, FloorHeight, 2]} />
-      <BlenderObject src="/models/floor.glb" position={[4, FloorHeight, 2]} />
-      <BlenderObject src="/models/floor.glb" position={[-4, FloorHeight, 4]} />
-      <BlenderObject src="/models/floor.glb" position={[-2, FloorHeight, 4]} />
-      <BlenderObject src="/models/floor.glb" position={[0, FloorHeight, 4]} />
-      <BlenderObject src="/models/floor.glb" position={[2, FloorHeight, 4]} />
-      <BlenderObject src="/models/floor.glb" position={[4, FloorHeight, 4]} />
+      <BlenderObject src="/models/FLOOR.glb" position={[-4, FloorHeight, 0]} />
+      <BlenderObject src="/models/FLOOR.glb" position={[-2, FloorHeight, 0]} />
+      <BlenderObject src="/models/FLOOR.glb" position={[0, FloorHeight, 0]} />
+      <BlenderObject src="/models/FLOOR.glb" position={[2, FloorHeight, 0]} />
+      <BlenderObject src="/models/FLOOR.glb" position={[4, FloorHeight, 0]} />
+      <BlenderObject src="/models/FLOOR.glb" position={[-4, FloorHeight, 2]} />
+      <BlenderObject src="/models/FLOOR.glb" position={[-2, FloorHeight, 2]} />
+      <BlenderObject src="/models/FLOOR.glb" position={[0, FloorHeight, 2]} />
+      <BlenderObject src="/models/FLOOR.glb" position={[2, FloorHeight, 2]} />
+      <BlenderObject src="/models/FLOOR.glb" position={[4, FloorHeight, 2]} />
+      <BlenderObject src="/models/FLOOR.glb" position={[-4, FloorHeight, 4]} />
+      <BlenderObject src="/models/FLOOR.glb" position={[-2, FloorHeight, 4]} />
+      <BlenderObject src="/models/FLOOR.glb" position={[0, FloorHeight, 4]} />
+      <BlenderObject src="/models/FLOOR.glb" position={[2, FloorHeight, 4]} />
+      <BlenderObject src="/models/FLOOR.glb" position={[4, FloorHeight, 4]} />
     </group>
   );
 };
