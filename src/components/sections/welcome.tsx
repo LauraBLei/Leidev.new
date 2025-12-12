2e5a8784fa8dfe458b9d9737a64432aaf5bffb69
LauraBLei <laura.blei@outlook.com>
2025-12-12 13:04:39 +0100

    added sign to window img

--- src/components/sections/welcome.tsx
+++ src/components/sections/welcome.tsx
@@ -1,16 +1,29 @@
+import { useState, useEffect } from "react";
+
 export const WelcomeSection = () => {
+  const [scrollY, setScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => setScrollY(window.scrollY);
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <section id="welcome" className="relative w-full">
-      <div className="relative w-full h-screen">
-        <div className="overflow-hidden z-0 absolute w-full h-full">
+      <div className="relative w-full h-screen overflow-hidden">
+        <div
+          className="absolute w-full h-full"
+          style={{ transform: `translateY(${scrollY * 0.5}px)` }}
+        >
           <img
             src="/images/mountain.jpg"
             className="w-full h-full object-cover"
           />
         </div>
-        <div className="overflow-hidden z-0 absolute w-full h-full">
+        <div className="z-10 absolute w-full h-full">
           <img
-            src="/images/window.png"
+            src="/images/window_wSign.png"
             className="w-full h-full object-cover"
           />
         </div>
